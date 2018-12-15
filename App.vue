f3149f3831f5af4a7d9628b37a05c3123234e47a
Puinun <Puinun.2015@gmail.com>
2018-12-15 08:26:35 +0700

    add code

---
+++ App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-warning">
+      <div class= "nav bavbar-bar">
+      <router-link to="/" class="nav-item nav-link active">Home</router-link> |
+      <router-link to="/about"  class="nav-item nav-link">About</router-link>|
+      <router-link to="/products"  class="nav-item nav-link">Products</router-link>|
+    <router-link to="/users"  class="nav-item nav-link">Users</router-link>|
+    <router-link to="/Grap"  class="nav-item nav-link">Grap</router-link>
+
+    </div>
+    </nav>
+    <router-view/>
+    <a href="http://localhost/app/products/">Product</a>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
